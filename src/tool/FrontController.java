87adce379bee4302a3806e27acfcab88724ebbee
k_kazuyoshi <k_kazuyoshi@192.168.227.121>
2024-07-01 11:56:41 +0900

    a

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -1,4 +1,3 @@
-//FrontController.java
 package tool;
 
 import java.io.IOException;
@@ -10,28 +9,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"*.action"})
+
 public class FrontController extends HttpServlet {
 
 	@Override
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
 		try {
+
 			String path = req.getServletPath().substring(1);
+
 			String name = path.replace(".a", "A").replace('/', '.');
 
 			System.out.println("★ servlet path -> " + req.getServletPath());
+
 			System.out.println("★ class name -> " + name);
 
 			Action action = (Action) Class.forName(name).getDeclaredConstructor().newInstance();
 
 			action.execute(req, res);
+
 		} catch (Exception e) {
+
 			e.printStackTrace();
+
 			req.getRequestDispatcher("error.jsp").forward(req, res);
+
 		}
+
 	}
 
 	@Override
+
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
 		doGet(req, res);
+
 	}
+
 }
