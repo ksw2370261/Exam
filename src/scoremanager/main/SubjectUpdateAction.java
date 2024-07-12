af8ece29255e171779c0c0a3145a3242aa306e50
t_takahashi <t_takahashi@192.168.227.154>
2024-07-12 14:31:49 +0900

    遷移成功

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -2,19 +2,16 @@ package scoremanager.main;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/SubjectUpdate.action")
-public class SubjectUpdateAction extends tool.Action {
+import tool.Action;
+
+public class SubjectUpdateAction extends Action {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 科目変更の処理
-
         // 科目コードを取得
         String subjectCode = request.getParameter("subjectCode");
 
@@ -22,7 +19,6 @@ public class SubjectUpdateAction extends tool.Action {
         request.setAttribute("subjectCode", subjectCode); // 実際の科目データオブジェクトで置き換える
 
         // JSPにフォワードして科目変更フォームを表示
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/subject_update.jsp");
-        dispatcher.forward(request, response);
+        request.getRequestDispatcher("subject_update.jsp").forward(request, response);
     }
 }
